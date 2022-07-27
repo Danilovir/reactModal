7efa45b9db2840a4dcee2b35a7badc5464a6e258
Danilo Vieira <danilo.vieira7@fatec.sp.gov.br>
2022-07-27 15:09:24 -0300

    Primeiro Commit

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import{ useState } from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, Modal } from 'react-native';
+import { ActionModal } from './src/ActionModal'
 
 export default function App() {
+  const [visibleModal, setVisibleModal] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Sujeito Programador!</Text>
+      
+      <TouchableOpacity style={styles.button} onPress={ () => setVisibleModal(true) }>
+        <Text>Open</Text>
+      </TouchableOpacity>
+
+      <Modal
+        visible={visibleModal}
+        transparent={true}
+        onRequestClose={ () => setVisibleModal(false) }
+        animationType="slide"
+      >
+       
+        <ActionModal
+        handleClose={ () => setVisibleModal(false) }
+        handleEdit={ () => alert("clicou no editar") }
+        handleDelete={ () => alert("Clicou no excluir") }
+        />
+      </Modal>
+
     </View>
   );
 }
@@ -13,8 +34,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#12131B',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title:{
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+
+  button:{
+    backgroundColor: '#FFF',
+    padding: 4,
+    marginVertical: 8,
+    width: '70%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+
+  }
+
 });
